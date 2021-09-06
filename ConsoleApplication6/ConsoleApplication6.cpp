392de237415289da252dbd2c0d8054678b830abb
saleh muhammad <salehmalik121@gmail.com>
2021-09-07 02:02:45 +0500

    Add project files.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int shift(int* a, int* b, int* c) {
+	*a = *b;
+	*b = *c;
+	return 0;
+}
+void fib(int length) {
+	int n1 = 0;
+	int n2 = 1;
+	int n3 = 0;
+	for (int i = 0; i <= length; i++) {
+		cout << n1 << " ";
+		n3 = n2 + n1;
+		shift(&n1, &n2, &n3);
+	}
+}
+
+int main() {
+	int LengthOfSeq;
+	cout << "Enter length of series :" ;
+	cin >> LengthOfSeq;
+	fib(LengthOfSeq);
+	return 0;
+}
