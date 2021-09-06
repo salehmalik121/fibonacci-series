c7cf21ec829b8a03f1475c9b77b22ddc0cc90ff3
saleh muhammad <salehmalik121@gmail.com>
2021-09-07 02:06:08 +0500

    length fixed

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -9,7 +9,7 @@ void fib(int length) {
 	int n1 = 0;
 	int n2 = 1;
 	int n3 = 0;
-	for (int i = 0; i <= length; i++) {
+	for (int i = 0; i <= length-1; i++) {
 		cout << n1 << " ";
 		n3 = n2 + n1;
 		shift(&n1, &n2, &n3);
